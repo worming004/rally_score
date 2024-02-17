ed4fe655f8ecd919afa0dc5b0dc915fb0444c230
Mathieu Scolas <worming@hotmail.be>
2024-02-17 22:06:18 +0100

    mix credo

--- lib/rally_score_web/components/core_components.ex
+++ lib/rally_score_web/components/core_components.ex
@@ -16,6 +16,7 @@ defmodule RallyScoreWeb.CoreComponents do
   """
   use Phoenix.Component
 
+  alias Phoenix.HTML.Form
   alias Phoenix.LiveView.JS
   import RallyScoreWeb.Gettext
 
@@ -304,7 +305,7 @@ defmodule RallyScoreWeb.CoreComponents do
   def input(%{type: "checkbox"} = assigns) do
     assigns =
       assign_new(assigns, :checked, fn ->
-        Phoenix.HTML.Form.normalize_value("checkbox", assigns[:value])
+        Form.normalize_value("checkbox", assigns[:value])
       end)
 
     ~H"""
