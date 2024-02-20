375a14b19b133d17e4018bb7b055b14d0bbdd4fe
Mathieu Scolas <worming@hotmail.be>
2024-02-20 08:59:42 +0100

    apply db update

---
+++ priv/repo/migrations/20240220074637_add_admin_user.exs
@@ -0,0 +1,9 @@
+defmodule RallyScore.Repo.Migrations.AddAdminUser do
+  use Ecto.Migration
+
+  def change do
+    alter table(:users) do
+      add :is_admin, :boolean, default: false
+    end
+  end
+end
